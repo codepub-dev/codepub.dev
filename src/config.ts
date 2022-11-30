c1ea52ab7047b1931ad6a6c58b5374bd53985e30
Darko <dbozhinovski@gmail.com>
2022-11-30 16:16:30 +0100

    Fixes sizing

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'CodePub - the podcast about beers and code';
-export const SITE_DESCRIPTION = 'code on tap (the podcast about beer and code)';
+export const SITE_TITLE = 'CodePub - the podcast for nerds with refined tastes';
+export const SITE_DESCRIPTION = 'Code and beer for all.';
