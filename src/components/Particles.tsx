ae300cc5e57e18da2a6460ce480ba0303d9baf16
Darko <dbozhinovski@gmail.com>
2023-02-03 15:56:15 +0100

    Fixes v scrollbar

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -2,4 +2,4 @@ import { loadFull } from 'tsparticles';
 import Particles from "solid-particles";
 import { bubbles } from '../constants/particles/bubbles';
 
-export const Bubbles = () => <Particles id="tsparticles" init={async (main) => await loadFull(main)} options={bubbles} className="absolute top-0 h-screen w-full" />;
+export const Bubbles = () => <Particles id="tsparticles" init={async (main) => await loadFull(main)} options={bubbles} className="z-10" />;
